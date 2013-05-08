df197856d3bd38f31949ab86e4c21484d4d9e866
Sen Wang <moli.wang@gmail.com>
2013-05-08 09:26:49 +0800

    init

---
+++ kinectsample_tiltcontrol/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+
+namespace kinectsample_tiltcontrol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KinectSensor sensor = KinectSensor.KinectSensors[0];
+            sensor.Start();
+
+            sensor.ElevationAngle = 0; 
+
+            sensor.Stop();
+
+        }
+    }
+}
