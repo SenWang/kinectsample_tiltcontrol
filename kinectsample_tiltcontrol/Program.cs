2d431a6f607cf50c977d06f68f85c8ef867c78ec
Sen Wang <moli.wang@gmail.com>
2013-05-08 09:30:45 +0800

    動態改變 Kinect俯仰角

--- kinectsample_tiltcontrol/Program.cs
+++ kinectsample_tiltcontrol/Program.cs
@@ -14,7 +14,22 @@ namespace kinectsample_tiltcontrol
             KinectSensor sensor = KinectSensor.KinectSensors[0];
             sensor.Start();
 
-            sensor.ElevationAngle = 0; 
+            sensor.ElevationAngle = 0;  // 一開始先設定成水平0度
+
+            ConsoleKey presskey;
+            while ((presskey = Console.ReadKey().Key) != ConsoleKey.Spacebar)
+            {
+                if (presskey == ConsoleKey.DownArrow)
+                {
+                    sensor.ElevationAngle = sensor.ElevationAngle - 5;
+                }
+                else if (presskey == ConsoleKey.UpArrow)
+                {
+                    sensor.ElevationAngle = sensor.ElevationAngle + 5;
+                }
+                Console.WriteLine("現在角度 : " + sensor.ElevationAngle);
+            }
+
 
             sensor.Stop();
 
